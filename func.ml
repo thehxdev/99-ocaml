3ed99ac35433b5fd07541880ecbd782279348763
Hossein Khosravi <93550458+thehxdev@users.noreply.github.com>
2023-03-19 11:37:13 +0330

    Add files via upload

---
+++ func.ml
@@ -0,0 +1,34 @@
+(*
+open Stdio
+*)
+open Base
+
+
+let even (x: int) : bool =
+    x % 2 = 0;;
+
+
+let rec range (bottom: int) (top: int) : int list =
+    match bottom >= top with
+    | true -> []
+    | false -> bottom :: range (bottom + 1) top;;
+
+
+let rec filter (f: 'a -> bool) (l: 'a list) : 'a list =
+    match l with
+    | [] -> []
+    | x :: rest -> 
+            match f x with
+            | true -> x :: filter f rest
+            | false -> filter f rest;;
+
+
+let rec map (f: 'a -> 'b) (l: 'a list) : 'b list =
+    match l with
+    | [] -> []
+    | x :: rest -> f x :: map f rest;;
+
+
+(*
+let () = Stdio.print_endline "Hello, World!"
+*)
